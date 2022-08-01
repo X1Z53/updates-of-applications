ccdf2b5014b2bf437834dc108764b7eafe6fae0f
X1Z53 <47402692+X1Z53@users.noreply.github.com>
2022-08-02 00:00:41 +0300

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,36 +1,56 @@
 import requests
 from bs4 import BeautifulSoup as BS
+from time import sleep
 
 
 class Program:
-    def __init__(self, name, downloaded_versions, parametrs):
-        self.name      = name
-        self.parametrs = parametrs or None
+    def __init__(self, name, downloaded_versions, parametrs=None):
+        self.name            = name
+        self.error           = ''
+        self.current_version = 0
 
-        if self.parametrs:
-            self.url           = self.parametrs.setdefault('url',           '')
-            self.download_link = self.parametrs.setdefault('download_link', self.url)
-            self.content       = self.parametrs.setdefault('content',       '')
-            self.word          = self.parametrs.setdefault('word',          0)
-            self.slice_start   = self.parametrs.setdefault('slice_start',   None)
-            self.slice_end     = self.parametrs.setdefault('slice_end',     None)
-            self.split_symbol  = self.parametrs.setdefault('split_symbol',  None) 
-            self.addons        = self.parametrs.setdefault('addons',        {})
+        if parametrs:
+            self.url           = parametrs.setdefault('url',           '')
+            self.download_link = parametrs.setdefault('download_link', self.url)
+            self.content       = parametrs.setdefault('content',       '')
+            self.word          = parametrs.setdefault('word',          0)
+            self.slice_start   = parametrs.setdefault('slice_start',   None)
+            self.slice_end     = parametrs.setdefault('slice_end',     None)
+            self.split_symbol  = parametrs.setdefault('split_symbol',  None)
+            self.addons        = parametrs.setdefault('addons',        {})
             
-            session = requests.get(self.url)
-            html = BS(session.text, 'html.parser')
-            if type(self.content) == bool: print(html)
-            if type(self.word) != bool and self.content:
-                self.current_version = html.select(self.content)[0].text.split(self.split_symbol)[self.word][self.slice_start:self.slice_end]
-            elif self.content:
-                self.current_version = ' '.join(html.select(self.content)[0].text.split(self.split_symbol)[self.slice_start:self.slice_end])
-            else:
-                self.current_version = 'Not Found'
-
-            self.current_version = self.current_version.replace('(', '').replace(')', '')
+            self.current_version_check()
         else:
-            self.current_version = 'Not Found'
+            self.error = 'No parametrs'
        
         self.downloaded_version = downloaded_versions[self.name.replace("'", '')] if self.name.replace("'", '') in downloaded_versions.keys() else 'Not Found'
 
-        self.have_update = self.downloaded_version != self.current_version
+        self.have_update = all((self.downloaded_version != self.current_version, not self.error))
+
+    def current_version_check(self):
+        try:
+            session = requests.get(self.url)
+        except:
+            self.error = 'No response to the request'
+        else:
+            html = BS(session.text, 'html.parser')
+            if type(self.content) == bool:
+                self.error = 'Show HTML Code'
+                self.current_version = html
+
+            element_response = html.select(self.content)
+
+            if all([self.content, element_response]):
+                text = element_response[0].text.split(self.split_symbol)
+
+                if type(self.word) != bool:
+                    try:
+                        self.current_version = text[self.word][self.slice_start:self.slice_end]
+                    except:
+                        self.error = f'Incorrect response or parametrs: {text, self.word, self.slice_start, self.slice_end}'
+                else:
+                    self.current_version = ' '.join(text[self.slice_start:self.slice_end])
+            else:
+                self.error = f'Incorrect response or parametrs: {self.content, element_response}'
+
+        if self.error and not self.current_version: self.current_version = 0
--- functions.py
+++ functions.py
@@ -24,7 +24,6 @@ def configure_list(unconfigured_list):
 
 def creating_objects(items_list, downloaded_versions, section=''):
     full_list = {}
-
     bar = Bar('Scanning updates for ' + section, max=len(items_list))
     
     for item_name, parametrs in items_list.items():
@@ -33,7 +32,7 @@ def creating_objects(items_list, downloaded_versions, section=''):
         else:
             item = Program(item_name, downloaded_versions, parametrs)
         
-            if item.parametrs and item.addons: item.addons = creating_objects(item.addons, downloaded_versions)
+            if item.addons: item.addons = creating_objects(item.addons, downloaded_versions)
 
         if section: bar.next()
         full_list[item_name] = item
@@ -46,12 +45,23 @@ def show_versions(section_list, prefix_number=0):
     for number, item in enumerate(section_list.items()):
         if hasattr(item[1], 'name'):
             item = item[1]
-            print(f'{"  " * prefix_number}{number+1}. {item.name}: {item.downloaded_version} {f"(New version - {item.current_version})" if item.have_update else ""}')
+            print(f'{"  " * prefix_number}{number+1}. {item.name}: {item.downloaded_version} {f"(Error: {item.error})" if item.error else f"(New version: {item.current_version})" if item.have_update else ""}')
             if hasattr(item, 'addons') and item.addons: show_versions(item.addons, prefix_number+1)
         else:
             print(f'{"  " * prefix_number}{number+1}. {item[0]}:')
             show_versions(item[1], prefix_number+1)
 
+def scan(root):
+    full_list = {}
+    walk = os.walk(root)
+
+    for path, folders, files in walk:
+        if '_' in path: continue
+        for file in files:
+            name, version = file.split(').')[0].split(' (', 1)
+            full_list[name] = version
+
+    return full_list
 
 def get_downloaded_versions(sections):
     path_separator = ['/', '\\'][platform=='win32']
@@ -59,26 +69,18 @@ def get_downloaded_versions(sections):
     downloaded_versions = scan(root)
     return downloaded_versions
 
-def scan(root):
-    full_list = {}
-    for path, folders, files in os.walk(root):
-        if '_' in path: continue
-        for file in files:
-            name, version = file.split(').')[0].split(' (')
-            full_list[name] = version
-
-    return full_list
-
 def for_update(full_list):
     update_list = []
     for item in full_list.values():
-        if not hasattr(item, 'name'): [update_list.append(i) for i in for_update(item)]
+        if not hasattr(item, 'name'):
+            [update_list.append(i) for i in for_update(item)]
         elif item.have_update: update_list.append(item)
+
     return update_list
 
 def show_updates(full_list):
     update_list = for_update(full_list)
     
-    if update_list and input('\nShow update links? [Y/N] ').lower() == 'y':
+    if update_list and input('\nShow update links? [Y/N] ').lower() != 'n':
         for number, item in enumerate(update_list):
-            print(f'{number+1}. {item.name} {item.current_version} - {item.download_link}')
+            print(f'{number+1}. {item.name} ({item.current_version}) - {item.download_link}')
